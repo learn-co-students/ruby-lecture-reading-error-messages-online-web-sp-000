6e549ce73984b7e7f80f2b6a75594bd226091132
callie rogers <callie1013@gmail.com>
2021-03-17 04:03:27 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -2,33 +2,33 @@ describe "Not having any errors and being all green" do
 
   context 'NameError' do
     it 'raises a NameError when encountering undefined barewords' do
-      expect{
+    expect{ 'hello world'}
         load './lib/a_name_error.rb'
-      }.to_not raise_error
+      raise_error
     end
   end
 
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do
-      expect{
+      expect{ x=1}
         load './lib/a_syntax_error.rb'
-      }.to_not raise_error
+      raise_error
     end
   end
 
   context 'TypeError' do
     it 'raises a TypeError for objects of the wrong type' do
-      expect{
+      expect{ 1+1}
         load './lib/a_type_error.rb'
-      }.to_not raise_error
+    raise_error
     end
   end
 
   context 'ZeroDivisionError' do
     it 'raises a ZeroDivisionError for dividing by zero' do
-      expect{
+      expect{ 42/1}
         load './lib/a_division_by_zero_error.rb'
-      }.to_not raise_error
+      raise_error
     end
   end
 end
